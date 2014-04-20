d83a372b20851b09010733b7d223a2be810e8929
Hyeon Kim <simnalamburt@gmail.com>
2014-04-21 08:54:13 +0900

    Made synchronous TCP server
    "Hello, World" 문자열을 송출하는 에코서버 작성

--- example/main.cpp
+++ example/main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include <string>
+#include <boost/asio.hpp>
 
 
 using namespace std;
+using namespace boost::asio;
+using           boost::asio::ip::tcp;
+
 
 int main()
 {
-  cout << "Hello, World!" << endl;
+  const unsigned short port = 4567;
+
+  cout << "Starting tcp server at \033[34m0.0.0.0:" << port << "\033[0m" << endl;
+  cout << "Check this out with the command below" << endl;
+  cout << endl;
+  cout << " $ \033[4;34mnc 127.0.0.1 " << port << "\033[0m" << endl;
+  cout << endl;
+
+  try
+  {
+    io_service service;
+    auto acceptor = tcp::acceptor(service, tcp::endpoint(tcp::v4(), port));
+    for(;;)
+    {
+      tcp::socket socket(service);
+      acceptor.accept(socket);
+
+      const auto endpoint = socket.remote_endpoint();
+      const auto client = "\033[32m" + endpoint.address().to_string() + ':' + to_string(endpoint.port()) + "\033[0m";
+      cout << client << " has been connected" << endl;
+      cout << endl;
+
+      write(socket, buffer("Hello, World!\n"));
+    }
+  }
+  catch (exception& e)
+  {
+    cerr << "\033[31mException has been thrown\033[0m" << endl;
+    cerr << "\033[33m" << e.what() << "\033[0m" << endl;
+    cerr << endl;
+  }
+  cout << "Bye!" << endl;
 }
