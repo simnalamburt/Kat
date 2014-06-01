46acbb878e051f01b19062c618c5beed1bbe933f
Hyeon Kim <simnalamburt@gmail.com>
2014-06-01 23:07:21 +0900

    Moved typedefs into 'types.h'
    Makefile 수정

--- example/main.cpp
+++ example/main.cpp
@@ -2,40 +2,13 @@
 #include <string>
 #include <thread>
 #include <boost/asio.hpp>
+#include "types.h"
 
 
 using namespace std;
 using namespace boost::asio;
 using           boost::asio::ip::tcp;
 using namespace boost::asio::error;
-
-
-using byte = unsigned char;
-using sbyte = signed char;
-//    char
-using wchar = wchar_t;
-//    short
-using ushort = unsigned short;
-//    int
-using uint = unsigned int;
-using intptr = intptr_t;
-using uintptr = uintptr_t;
-//    long
-using ulong = unsigned long;
-using llong = long long;
-using ullong = unsigned long long;
-
-using int8 = int8_t;
-using int16 = int16_t;
-using int32 = int32_t;
-using int64 = int64_t;
-using int128 = __int128_t;
-
-using uint8 = uint8_t;
-using uint16 = uint16_t;
-using uint32 = uint32_t;
-using uint64 = uint64_t;
-using uint128 = __uint128_t;
 
 
 const auto port = ushort(4567);
---
+++ example/types.h
@@ -0,0 +1,33 @@
+#pragma once
+
+//    bool
+using byte = unsigned char;
+using sbyte = signed char;
+//    char
+using wchar = wchar_t;
+//    short
+using ushort = unsigned short;
+//    int
+using uint = unsigned int;
+using intptr = intptr_t;
+using uintptr = uintptr_t;
+//    long
+using ulong = unsigned long;
+using llong = long long;
+using ullong = unsigned long long;
+
+using int8 = int8_t;
+using int16 = int16_t;
+using int32 = int32_t;
+using int64 = int64_t;
+using int128 = __int128_t;
+
+using uint8 = uint8_t;
+using uint16 = uint16_t;
+using uint32 = uint32_t;
+using uint64 = uint64_t;
+using uint128 = __uint128_t;
+
+//    float
+//    double
+using ldouble = long double;
