2c1f920c6ca0e3146888f5a3ae582426ec7a390c
Hyeon Kim <simnalamburt@gmail.com>
2014-06-01 22:21:08 +0900

    Added multithread support via std::thread
    std::thread 로 멀티스레딩 지원

--- example/main.cpp
+++ example/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <thread>
 #include <boost/asio.hpp>
 
 
@@ -45,35 +46,37 @@ Content-Type: text/html;charset=utf-8
       tcp::socket socket(service);
       acceptor.accept(socket);
 
-      const auto endpoint = socket.remote_endpoint();
-      const auto client = "\033[32m" + endpoint.address().to_string() + ':' + to_string(endpoint.port()) + "\033[0m";
-      cout << client << " has been connected" << endl;
-      cout << endl;
-
-      do
-      {
-        auto delim = "\r\n\r\n"s;
-
-        boost::asio::streambuf request_buffer;
-        boost::system::error_code error;
-        read_until(socket, request_buffer, delim, error);
-        if (error == eof)
-          break; // Connection closed cleanly by peer.
-        else if (error)
-          throw boost::system::system_error(error); // Some other error.
-
-        cout << client << " has sent a request" << endl;
-        cout << "\033[33m" << string(
-            buffer_cast<const char*>(request_buffer.data()),
-            request_buffer.size() - delim.size()) << "\033[0m" << endl;
+      thread([&](auto socket) {
+        const auto endpoint = socket.remote_endpoint();
+        const auto client = "\033[32m" + endpoint.address().to_string() + ':' + to_string(endpoint.port()) + "\033[0m";
+        cout << client << " has been connected" << endl;
         cout << endl;
 
-        write(socket, buffer(message));
-      }
-      while (false);
+        do
+        {
+          auto delim = "\r\n\r\n"s;
 
-      cout << client << " closed her connection gracefully" << endl;
-      cout << endl;
+          boost::asio::streambuf request_buffer;
+          boost::system::error_code error;
+          read_until(socket, request_buffer, delim, error);
+          if (error == eof)
+            break; // Connection closed cleanly by peer.
+          else if (error)
+            throw boost::system::system_error(error); // Some other error.
+
+          cout << client << " has sent a request" << endl;
+          cout << "\033[33m" << string(
+              buffer_cast<const char*>(request_buffer.data()),
+              request_buffer.size() - delim.size()) << "\033[0m" << endl;
+          cout << endl;
+
+          write(socket, buffer(message));
+        }
+        while (false);
+
+        cout << client << " closed her connection gracefully" << endl;
+        cout << endl;
+      }, move(socket)).detach();
     }
   }
   catch (exception& e)
