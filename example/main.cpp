e533efd611d0a9bd6e2746995c510bbaf0c1bf6e
Hyeon Kim <simnalamburt@gmail.com>
2014-06-09 01:58:19 +0900

    Added coroutine instead of class 'server'
    server 클래스를 삭제하고, 대신 같은 역할을 하는 코드를 코루틴으로
    만들어 main()에 집어넣음

--- example/main.cpp
+++ example/main.cpp
@@ -20,6 +20,7 @@ using           boost::intrusive_from_raw;
 using boost_error = boost::system::error_code;
 
 
+constexpr auto port = ushort(4567);
 constexpr auto max_length = size_t(8192);
 const     auto message = R"(HTTP/1.1 200 OK
 Server: Kat
@@ -39,91 +40,55 @@ Content-Type: text/html;charset=utf-8
 )"s;
 
 
-class server
-{
-  private:
-    constexpr static auto default_port = ushort(4567);
-
-    io_service _service;
-    tcp::acceptor _acceptor;
-    tcp::socket _socket;
-
-
-  public:
-    server(ushort port = default_port) :
-      _acceptor(_service, tcp::endpoint(tcp::v4(), port)),
-      _socket(_service)
-    {
-      accept();
-
-      cout << "Starting tcp server at \033[34m0.0.0.0:" << port << "\033[0m" << endl
-        << "Check this out with the command below" << endl
-        << endl
-        << " $ \033[4;34mwget http://127.0.0.1:" << port << "\033[0m" << endl
-        << endl;
-    }
-
-    ~server()
-    {
-      cout << "Bye!" << endl;
-    }
-
-    auto run() -> void
-    {
-      try
-      {
-        _service.run();
-      }
-      catch (exception& e)
-      {
-        cerr << "\033[31mException has been thrown\033[0m" << endl;
-        cerr << "\033[33m" << e.what() << "\033[0m" << endl;
-        cerr << endl;
-      }
-    }
-
-
-  private:
-    auto accept() -> void
-    {
-      _acceptor.async_accept(_socket, [this](boost::system::error_code error)
-      {
-        if (error) return; // TODO : 에러처리
-
-        spawn(_service, [&](auto yield)
-        {
-          auto socket = move(_socket);
-          auto client = socket.remote_endpoint();
-
-          cout << to_string(client) << " has been connected" << endl;
-          cout << endl;
-
-          array<byte, max_length> request;
-          boost_error error;
-
-          socket.async_read_some(buffer(request), yield[error]);
-          if (error) return; // TODO : 에러처리
-
-          cout << to_string(client) << " has sent a request" << endl;
-          cout << "\033[33m" << string(request.begin(), request.end()) << "\033[0m" << endl;
-
-          async_write(socket, buffer(message), yield[error]);
-          if (error) return; // TODO : 에러처리
-
-          socket.shutdown(tcp::socket::shutdown_both, error);
-
-          cout << to_string(client) << " close her connection gracefully" << endl;
-          cout << endl;
-        });
-
-        accept();
-      });
-    }
-};
-
-
 auto main() -> int
 {
-  server app;
-  app.run();
+  io_service service;
+  auto acceptor = tcp::acceptor(service, tcp::endpoint(tcp::v4(), port));
+  auto socket = tcp::socket(service);
+
+  spawn(service, [&](auto yield)
+  {
+    while (true)
+    {
+      boost_error error;
+      acceptor.async_accept(socket, yield[error]);
+      if (error) continue; // TODO : 에러처리
+
+      spawn(service, [&](auto yield)
+      {
+        auto client = move(socket);
+        auto info = to_string(client.remote_endpoint());
+
+        cout << info << " has been connected" << endl;
+        cout << endl;
+
+        array<byte, max_length> request;
+        boost_error error;
+
+        client.async_read_some(buffer(request), yield[error]);
+        if (error) return; // TODO : 에러처리
+
+        cout << info << " has sent a request" << endl;
+        cout << "\033[33m" << string(request.begin(), request.end()) << "\033[0m" << endl;
+
+        async_write(client, buffer(message), yield[error]);
+        if (error) return; // TODO : 에러처리
+
+        client.shutdown(tcp::socket::shutdown_both, error);
+
+        cout << info << " close her connection gracefully" << endl;
+        cout << endl;
+      });
+    }
+  });
+
+  cout << "Starting tcp server at \033[34m0.0.0.0:" << port << "\033[0m" << endl
+    << "Check this out with the command below" << endl
+    << endl
+    << " $ \033[4;34mwget http://127.0.0.1:" << port << "\033[0m" << endl
+    << endl;
+
+  service.run();
+
+  cout << "Bye!" << endl;
 }
