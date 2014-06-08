a425b84ab96310a77d020340be6a20b765daa2ae
Hyeon Kim <simnalamburt@gmail.com>
2014-06-09 00:54:58 +0900

    Added coroutine instead of class 'connection'
    'connection' 클래스를 삭제하고, 그 대신 같은 역할을 하는
    코루틴을 만들어 server 클래스 안에 람다함수로 집어넣음

--- example/main.cpp
+++ example/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <array>
 #include <boost/asio.hpp>
+#include <boost/asio/spawn.hpp>
 #include <boost/intrusive_ptr.hpp>
 #include <boost/smart_ptr/intrusive_ref_counter.hpp>
 
@@ -16,6 +17,7 @@ using namespace boost::asio::error;
 using           boost::intrusive_ptr;
 using           boost::intrusive_ref_counter;
 using           boost::intrusive_from_raw;
+using boost_error = boost::system::error_code;
 
 
 constexpr auto max_length = size_t(8192);
@@ -35,64 +37,6 @@ Content-Type: text/html;charset=utf-8
 </body>
 </html>
 )"s;
-
-
-class connection : public intrusive_ref_counter<connection>
-{
-  private:
-    tcp::socket _socket;
-    tcp::endpoint _client;
-    array<byte, max_length> _request;
-
-
-  public:
-    connection(tcp::socket&& socket) :
-      _socket(move(socket)),
-      _client(_socket.remote_endpoint())
-    {
-      cout << to_string(_client) << " has been connected" << endl;
-      cout << endl;
-
-      read();
-    }
-
-    ~connection()
-    {
-      cout << to_string(_client) << " close her connection gracefully" << endl;
-      cout << endl;
-    }
-
-
-  private:
-    auto read() -> void
-    {
-      auto self = intrusive_from_raw(this);
-
-      _request.fill(0);
-      _socket.async_read_some(buffer(_request), [this, self](boost::system::error_code error, size_t len)
-      {
-        if (error) return; // TODO : 에러처리
-
-        cout << to_string(_client) << " has sent a request" << endl;
-        cout << "\033[33m" << string(_request.begin(), _request.end()) << "\033[0m" << endl;
-
-        write();
-      });
-    }
-
-    auto write() -> void
-    {
-      auto self = intrusive_from_raw(this);
-
-      async_write(_socket, buffer(message), [this, self](boost::system::error_code error, size_t)
-      {
-        if (error) return; // TODO : 에러처리
-
-        boost::system::error_code ignored;
-        _socket.shutdown(tcp::socket::shutdown_both, ignored);
-      });
-    }
-};
 
 
 class server
@@ -146,7 +90,31 @@ class server
       {
         if (error) return; // TODO : 에러처리
 
-        auto ptr = intrusive_from_raw(new connection(move(_socket)));
+        spawn(_service, [&](auto yield)
+        {
+          auto socket = move(_socket);
+          auto client = socket.remote_endpoint();
+
+          cout << to_string(client) << " has been connected" << endl;
+          cout << endl;
+
+          array<byte, max_length> request;
+          boost_error error;
+
+          socket.async_read_some(buffer(request), yield[error]);
+          if (error) return; // TODO : 에러처리
+
+          cout << to_string(client) << " has sent a request" << endl;
+          cout << "\033[33m" << string(request.begin(), request.end()) << "\033[0m" << endl;
+
+          async_write(socket, buffer(message), yield[error]);
+          if (error) return; // TODO : 에러처리
+
+          socket.shutdown(tcp::socket::shutdown_both, error);
+
+          cout << to_string(client) << " close her connection gracefully" << endl;
+          cout << endl;
+        });
 
         accept();
       });
